fc9a8a5bec6bd85b8fdbf21c89d23e7f0d1845d2
seungho Jeon <urijsh612@gmail.com>
2019-12-04 17:18:51 +0900

    #4.2 React Router

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,16 +2,15 @@ import React from "react";
 import GlobalStyles from "../Styles/GlobalStyles";
 import styled, { ThemeProvider } from "styled-components";
 import Theme from "../Styles/Theme";
-
-const H2 = styled.h2`
-  color: ${props => props.theme.darkGreyColor};
-`;
+import Router from "./Router";
 
 export default () => {
   return (
     <ThemeProvider theme={Theme}>
-      <GlobalStyles />
-      <H2>안녕</H2>
+      <>
+        <GlobalStyles />
+        <Router isLoggedIn={!false} />
+      </>
     </ThemeProvider>
   );
 };
