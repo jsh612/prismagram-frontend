c6f150172ee9573f3209542aa5f9701e7dc32029
seungho Jeon <urijsh612@gmail.com>
2019-12-04 19:01:21 +0900

    #4.3 Apollo Client

---
+++ src/Apollo/Client.js
@@ -0,0 +1,12 @@
+import ApolloClient from "apollo-boost";
+import { defaults, resolvers } from "./LocalState";
+
+export default new ApolloClient({
+  uri: "http://localhost:4000",
+  // cleintStae
+  // - https://www.apollographql.com/docs/link/links/state/#with-apollo-boost
+  clientState: {
+    defaults,
+    resolvers
+  }
+});
---
+++ src/Apollo/LocalState.js
@@ -0,0 +1,28 @@
+// 얍의 상태를 체크한다 (로그인 여부를 확인)
+
+export const defaults = {
+  // local state의 값 설정
+  //# window.localStorage
+  // : https://developer.mozilla.org/ko/docs/Web/API/Window/localStorage
+  isLoggedIn: localStorage.getItem("token") !== null ? true : false
+};
+
+export const resolvers = {
+  // 밑의 Mutation은 local state를 변경하는 역할
+  Mutation: {
+    logUserIn: (_, { token }, { cache }) => {
+      localStorage.setItem("token", token);
+      cache.writeData({
+        data: {
+          isLoggedIn: true
+        }
+      });
+      return null;
+    },
+    logUserOut: (_, __, { cache }) => {
+      localStorage.removeItem("token");
+      window.location.reload(); //기존 캐시를 삭제하고 새로고침
+      return null;
+    }
+  }
+};
