bd08d331f6571afb5d1f38a9a69f186f1c459615
seungho Jeon <urijsh612@gmail.com>
2019-12-05 15:12:44 +0900

    #5.3 Auth Form with Hooks

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import Input from "../Components/Input";
 import Button from "../Components/Button";
+import useInput from "../Hooks/useInput";
 
 const Wrapper = styled.div`
   min-height: 80vh;
@@ -46,22 +47,27 @@ const Form = styled(Box)`
 
 export default () => {
   const [action, setAction] = useState("logIn");
+  const username = useInput("");
+  const password = useInput("");
+  const firstName = useInput("");
+  const lastName = useInput("");
+  const email = useInput("");
   return (
     <Wrapper>
       <Form>
         {action === "logIn" ? (
           <form>
-            <Input placeholder={"Username"} />
-            <Input placeholder={"Password"} />
+            <Input placeholder={"Username"} {...username} />
+            <Input placeholder={"Password"} {...password} type="password" />
             <Button text={"로그인"} />
           </form>
         ) : (
           <form>
-            <Input placeholder={"First name"} />
-            <Input placeholder={"Last name"} />
-            <Input placeholder={"Email"} />
-            <Input placeholder={"Username"} />
-            <Input placeholder={"Password"} />
+            <Input placeholder={"First name"} {...firstName} />
+            <Input placeholder={"Last name"} {...lastName} />
+            <Input placeholder={"Email"} {...email} type="email" />
+            <Input placeholder={"Username"} {...username} />
+            <Input placeholder={"Password"} {...password} type="password" />
             <Button text={"가입하기"} />
           </form>
         )}
